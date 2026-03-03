8f905ccd4ac4fc9a89f57dc70e80384431080e32
Youlia🦋 <182519876+youliakit@users.noreply.github.com>
2026-03-03 22:08:20 +0400

    Initial Commit

---
+++ Noted/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Noted
+//
+//  Created by 𝒴𝑜𝓊𝓁𝒾𝒶 𝒯𝒾𝑔𝓇𝒶𝓃𝓎𝒶𝓃 on 03.03.26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Noted/NotedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotedApp.swift
+//  Noted
+//
+//  Created by 𝒴𝑜𝓊𝓁𝒾𝒶 𝒯𝒾𝑔𝓇𝒶𝓃𝓎𝒶𝓃 on 03.03.26.
+//
+
+import SwiftUI
+
+@main
+struct NotedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
